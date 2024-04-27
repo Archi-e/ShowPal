d3614b927f8cc461ae9cd268e5f98ac72045f01e
Archie Tripathi <archie.webwork@gmail.com>
2024-04-27 05:54:00 +0530

    Started ShowPal

---
+++ ShowPal/src/main/java/com/archie/ShowPal/ShowPalApplication.java
@@ -0,0 +1,13 @@
+package com.archie.ShowPal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowPalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowPalApplication.class, args);
+	}
+
+}
---
+++ ShowPal/src/test/java/com/archie/ShowPal/ShowPalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archie.ShowPal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowPalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
